7cb820de62a6b46c3ee3aa1c134143de3037951e
Rogerio <rogeriodosanjosbarbosa2004@gmail.com>
2025-06-30 08:51:22 -0300

    Configurando arquivo settings.py e criando base de dados do projeto site

--- projeto_site/settings.py
+++ projeto_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7g(gm0&9r)s6e%mv8w0j#ow532i5l=v(=jw-(d)@l6e+ygg2+7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
