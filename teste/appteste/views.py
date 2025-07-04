6ff6a9c1d288b4c4040d34585ddd8d0678d195b2
Rogerio <rogeriodosanjosbarbosa2004@gmail.com>
2025-07-04 10:46:57 -0300

    Adicionando views e templates ao projeto teste

--- teste/appteste/views.py
+++ teste/appteste/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'appteste/post_list.html', {}) # Template que ainda será criado
