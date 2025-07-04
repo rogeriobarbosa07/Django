e3c61abf1f61c62b2987d34db42aac02a6a77dc9
Rogerio <rogeriodosanjosbarbosa2004@gmail.com>
2025-07-04 13:00:35 -0300

    Adicionando templates ao projeto teste

--- teste/appteste/views.py
+++ teste/appteste/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
 
-def post_list(request):
-    return render(request, 'appteste/post_list.html', {}) # Template que ainda será criado
+def post_list(request): 
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    return render(request, 'appteste/post_list.html', {'posts': posts})
