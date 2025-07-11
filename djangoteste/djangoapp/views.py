c158bdf36cb233c7c6bc21b7a5ed55b7d7d7a43d
Rogerio <rogeriodosanjosbarbosa2004@gmail.com>
2025-07-11 08:07:14 -0300

    Adicionando configuração de views e urls no djangoteste

--- djangoteste/djangoapp/views.py
+++ djangoteste/djangoapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the djangoapp index.")
+    # Para acessá-la em um navegador, precisamos mapeá-la para uma URL
