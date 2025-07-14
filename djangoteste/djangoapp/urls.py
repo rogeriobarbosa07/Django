a3d5f32edf2bd7a0e9680878694d13c565d1370e
Rogerio <rogeriodosanjosbarbosa2004@gmail.com>
2025-07-14 08:05:25 -0300

    Adicionando mais views e urls ao djangoteste

--- djangoteste/djangoapp/urls.py
+++ djangoteste/djangoapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("djangoapp/", views.detail, name="detail"),
+    path("djangoapp/results/", views.results, name="results"),
+    path("djangoapp/vote/", views.vote, name="vote"),
 ]
--- djangoteste/djangoapp/views.py
+++ djangoteste/djangoapp/views.py
@@ -6,3 +6,15 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the djangoapp index.")
     # Para acessá-la em um navegador, precisamos mapeá-la para uma URL
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
